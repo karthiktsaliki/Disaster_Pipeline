6a8a76740a0119b5850893a3a3383ebf09752b9c
Karthik Tsaliki <7karthikchowdary@gmail.com>
2018-09-20 19:44:15 +0530

    Final ML pipeline
    Added doc strings.

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,9 +1,6 @@
 import sys
 import pandas as pd
-import nltk
 import string
-import matplotlib.pyplot as plt
-import numpy as np
 import pickle
 from sqlalchemy import create_engine
 from nltk import word_tokenize
@@ -21,6 +18,11 @@ eng_stopwords = set(stopwords.words('english'))
 
 
 def load_data(database_filepath):
+     """Load the messages and categories merged dataframe from database
+    Uses pandas to read the table for reading dataset    
+    Arguments:
+    database_filepath -- Database file path
+    """
     engine = create_engine('sqlite:///'+database_filepath)
     df = pd.read_sql_table('DisasterResponse.db',engine)
     x=df.message
@@ -28,7 +30,14 @@ def load_data(database_filepath):
     return x,y,list(y.columns)
 
 def tokenize(text):
+    """Tokenize the given text.
+    Uses NLTK word_tokenize function to split the given text into words.
+    Uses WordNetLemmatizer of NLTK to convert the given list of words into their base form.
+    Removes punctuations from text
     
+    Arguments:
+    text -- Each message in messages dataset
+    """
     # Remove punctuations
     for key in string.punctuation:
         text=text.replace(key,' ')
@@ -45,11 +54,18 @@ def tokenize(text):
     return clean_tokens
 
 def build_model():
+    """Pipeline for creating model
+    Uses both TfidfVectorizer and MultiOutputClassifier for creating ML pipeline
+    Uses Grid Search for parameter tuning
+    Return CV object
+    """
     
+    # TfidfVectorizer and MultiOutputClassifier for creating ML pipeline
     pipeline = Pipeline([
     ('vectorizer', TfidfVectorizer(tokenizer=tokenize,stop_words=eng_stopwords)),
     ('clf', MultiOutputClassifier(OneVsRestClassifier(LinearSVC())))])
     
+    # Grid Search for parameter tuning
     parameters =  {
                     'clf__estimator__estimator__class_weight': ['balanced',None],
                     'clf__estimator__estimator__C': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
@@ -57,11 +73,20 @@ def build_model():
                   }
     
     cv =  GridSearchCV(pipeline, parameters) 
-    
+        
     return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
+    """Evaluates the created model on test data
+    Uses classification_report to print the accuracy, precision, recall
+    
+    Arguments:
+    model -- final model to evaluate test data
+    X_test -- independent data 
+    Y_test -- Variable which we have to predict
+    category_names -- list of categories
+    """
     test_preds=model.predict(X_test)
     for ind,cat in enumerate(category_names):
         print('For Category: ',cat,'\n classification report is \n')
@@ -76,10 +101,23 @@ def evaluate_model(model, X_test, Y_test, category_names):
 
 
 def save_model(model, model_filepath):
+    """save the best model
+    
+    Arguments:
+    model -- final model 
+    model_filepath -- path to save model
+    """
     pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
+    """Main method which covers
+     Loading data
+     Building model
+     Training model
+     Evaluating model
+     Saving model
+    """
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
