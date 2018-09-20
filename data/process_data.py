73b76a0ebcdcbc7792bf6e91f19c48ee448d1306
Karthik Tsaliki <7karthikchowdary@gmail.com>
2018-09-20 19:43:33 +0530

    Final ETL pipeline
    Added doc strings

--- data/process_data.py
+++ data/process_data.py
@@ -4,13 +4,31 @@ from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
+    """Load the messages and categories dataframe and return the merged dataframe
+    Uses pandas to read the messages and categories dataset
+    Merge the two datasets using id column
+    
+    Arguments:
+    messages_filepath -- Messages dataset file path
+    categories_filepath -- Categories dataset file path
+    """
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
     df=pd.merge(messages,categories,on='id')
     return df
 
 def clean_data(df):
+     """Cleans the dataframe categories this performs transform step ETL
+    Extracts a dataframe of the 36 individual category columns
+    Selects the first row of the categories dataframe
+    Set each value to be the last character of the string
+    Converts column from string to numeric
+    Concatenates the original dataframe with the new categories dataframe
+    Removing duplicates
     
+    Arguments:
+    df -- Merged dataframe
+    """
     # creating a dataframe of the 36 individual category columns
     categories = df.categories.str.split(';',expand=True)
     
@@ -39,11 +57,19 @@ def clean_data(df):
 
 
 def save_data(df, database_filename):
+     """Saves the cleaned data in database
+    
+    Arguments:
+    df -- cleaned dataframe
+    database_filename -- data will be stored in this database
+    """
     engine = create_engine('sqlite:///'+database_filename)
     df.to_sql(database_filename, engine, index=False)  
 
 
 def main():
+     """Main function which performs ETL on the datasets
+    """
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
